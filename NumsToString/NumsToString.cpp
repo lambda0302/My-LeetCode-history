ee4c4094d0b72490a0d3a5dc8c38a4a72c2759f4
easy <liyiyong501@gmail.com>
2022-05-17 18:37:39 +0800

    Create NumsToString.cpp
    增加了对字符串和数字转换的方法

---
+++ NumsToString/NumsToString.cpp
@@ -0,0 +1,51 @@
+﻿#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main() {											//数字转数字字符
+	int a;
+	char c;
+	cout << "please" << endl;
+	cin >> a;
+	c = a + '0';
+	cout << c << endl;
+	system("pause");
+	getchar();
+	return 0;
+
+
+}
+
+
+
+
+/*
+int main() {												//数字转数字字符串
+	int a;
+	//char c;
+	char* d = (char*)malloc(sizeof(char) * 4);
+	cout << "qingshuruzifushuzi" << endl;
+	cin >> a;
+	sprintf(d, "%d", a);
+	cout << d <<endl;
+
+
+	return 0;
+}
+*/
+/*
+int main() {										//数字字符串转数字
+	int a=0;
+	char *c = (char*)malloc(sizeof(char));
+	cout << "please" << endl;
+	cin >> c;
+	for (int i = 0; i < strlen(c); i++) {
+		a = a * 10 + (c[i] - '0');
+	}
+	
+
+	cout << a << endl;
+
+	return  0;
+}
+*/
